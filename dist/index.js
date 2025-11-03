e8dfaec2f01787d4bf63b4200a412b3b9af6e171
Alex Jamarco <alexandre.jamarco@eori.uk>
2025-11-03 16:02:21 +0000

    fix: update Babel configuration and clean up MoreAppsButton export

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,1 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "MoreAppsButton", {
-  enumerable: true,
-  get: function get() {
-    return _MoreAppsButton["default"];
-  }
-});
-var _MoreAppsButton = _interopRequireDefault(require("./MoreAppsButton"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+export { default as MoreAppsButton } from './MoreAppsButton';
