8c4478ccf938ae973d38d58bc02de7e9037ac883
Alex Jamarco <alexandre.jamarco@eori.uk>
2025-11-04 10:22:34 +0000

    fix: refactor MoreAppsButton component for improved readability and maintainability

--- src/MoreAppsButton.js
+++ src/MoreAppsButton.js
@@ -1,15 +1,40 @@
-import React, { useState } from 'react';
-import AppsIcon from '@mui/icons-material/Apps';
-import { IconButton, Menu, MenuItem } from '@mui/material';
-import ICS2Logo from './assets/images/cabie-ics2-logo.png';
-import ENSLogo from './assets/images/CABIE-ENS.png';
-import logo360Pay from './assets/images/360pay-grey-logo.png';
+import React, { useState } from "react";
+import AppsIcon from "@mui/icons-material/Apps";
+import { IconButton, Menu, MenuItem } from "@mui/material";
+import ICS2Logo from "./assets/images/cabie-ics2-logo.png";
+import ENSLogo from "./assets/images/CABIE-ENS.png";
+import logo360Pay from "./assets/images/360pay-grey-logo.png";
+
+const BUTTONS = [
+  {
+    logo: ICS2Logo,
+    alt: "CABIE ICS2 Logo",
+    style: {
+      height: "35px",
+      width: "auto",
+      marginTop: 22,
+      marginBottom: 22,
+    },
+    onClickURL: "https://ics2.cabie.uk/",
+  },
+  {
+    logo: ENSLogo,
+    alt: "CABIE ENS Logo",
+    style: {
+      height: "47px",
+      width: "auto",
+      marginTop: 22,
+      marginBottom: 22,
+    },
+    onClickURL: "https://ensgb.cabie.uk/",
+  },
+];
 
 const MoreAppsButton = () => {
   const [openMenu, setOpenMenu] = useState(null);
 
   const handleClick = (event) => {
-    console.log('Clicked More Apps', event.currentTarget);
+    console.log("Clicked More Apps", event.currentTarget);
     setOpenMenu(event.currentTarget);
   };
 
@@ -17,20 +42,21 @@ const MoreAppsButton = () => {
     setOpenMenu(null);
   };
 
-  const handleMenuItemClick = (option) => {
+  const handleMenuItemClick = (url) => {
+    //open the URL in a new tab
+    window.open(url, "_blank");
     handleClose();
-    // Add logic for clicking on each app option here
   };
 
   return (
     <>
       <IconButton
         onClick={handleClick}
-        aria-controls={openMenu ? 'apps-menu' : undefined}
+        aria-controls={openMenu ? "apps-menu" : undefined}
         aria-haspopup="true"
-        aria-expanded={openMenu ? 'true' : undefined}
+        aria-expanded={openMenu ? "true" : undefined}
       >
-        <AppsIcon sx={{ color: '#00395d', height: '40px', width: '40px' }} />
+        <AppsIcon sx={{ color: "#00395d", height: "40px", width: "40px" }} />
       </IconButton>
       <Menu
         id="apps-menu"
@@ -38,46 +64,36 @@ const MoreAppsButton = () => {
         open={!!openMenu}
         onClose={handleClose}
         MenuListProps={{
-          'aria-labelledby': 'apps-button',
+          "aria-labelledby": "apps-button",
         }}
       >
-        <div style={{ display: 'flex', justifyContent: 'center', borderBottom: '1px solid #eee' }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            borderBottom: "1px solid #eee",
+          }}
+        >
           <img
             src={logo360Pay}
             alt="CABIE 360Pay Logo"
             style={{
-              height: '35px',
-              width: 'auto',
+              height: "35px",
+              width: "auto",
               marginTop: 22,
               marginBottom: 22,
               marginLeft: 10,
             }}
           />
         </div>
-        <MenuItem onClick={() => handleMenuItemClick('test1')}>
-          <img
-            src={ICS2Logo}
-            alt="CABIE ICS2 Logo"
-            style={{
-              height: '35px',
-              width: 'auto',
-              marginTop: 22,
-              marginBottom: 22,
-            }}
-          />
-        </MenuItem>
-        <MenuItem onClick={() => handleMenuItemClick('test2')}>
-          <img
-            src={ENSLogo}
-            alt="CABIE ENS Logo"
-            style={{
-              height: '47px',
-              width: 'auto',
-              marginTop: 22,
-              marginBottom: 22,
-            }}
-          />
-        </MenuItem>
+        {BUTTONS.map((button, index) => (
+          <MenuItem
+            key={index}
+            onClick={() => handleMenuItemClick(button.onClickURL)}
+          >
+            <img src={button.logo} alt={button.alt} style={button.style} />
+          </MenuItem>
+        ))}
       </Menu>
     </>
   );
